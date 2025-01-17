d5e13926479ffc0fa7c0d456ddbb9f6024cddb01
hung-Liu <meps106148@gmail.com>
2025-01-17 22:21:06 +0800

    time

--- class.js
+++ class.js
@@ -1,15 +1,16 @@
-var yn = '';
-
-$('#name').on('click', function(){
-    console.log('a')
-    prompt('我的名字:')
-});
-
-function whenclick() {
-    yn = prompt('my name is_')
-    $('h1').text('my name is ???')
-    setTimeout(show, 1000);
-}
-function show(){
-    $('h1').text('my name is ' + yn)
-}
+const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+setInterval(() => {
+    const now = new Date();
+    const year = now.getFullYear(); 
+    const month = now.getMonth(); // 1 月是 1，所以需要加 1
+    const day = now.getDate();
+    const weekday = now.getDay();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    $("#time").text(hours+':'+minutes+':'+seconds)
+    $("#date").text(months[month]+' '+day+' '+year)
+}, 100);
