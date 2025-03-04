108dde17eb042fa7245ab6ec23f6a7fcca73d3c2
hung-Liu <meps106148@gmail.com>
2025-03-04 20:26:34 +0800

    update  template

---
+++ note/notes/code/template.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define int long long
+#define ll long long
+#define F first
+#define S second
+#define Yes cout<<"Yes\n"
+#define No cout<<"No\n"
+#define pii pair<ll,ll>
+#define hoshino ios::sync_with_stdio(0)
+#define orz cin.tie(0)
+#define osz cout.tie(0)
+
+inline void solve(){
+    
+}
+
+signed main(){
+    hoshino;
+    orz,osz;
+    int T = 1;
+    //cin>>T;
+    for(int i = 0; i<T; i++){
+        //cout<<"Case #<<i+1<<'\n';
+        solve();
+    }
+    return 0;
+}
+
