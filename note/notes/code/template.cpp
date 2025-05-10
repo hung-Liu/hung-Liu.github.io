cc72ce263549b1d94267c0abef3ab7e82391fe11
hung-Liu <meps106148@gmail.com>
2025-05-10 12:57:55 +0800

    update template

--- note/notes/code/template.cpp
+++ note/notes/code/template.cpp
@@ -4,14 +4,17 @@ using namespace std;
 #define pb push_back
 #define int long long
 #define ll long long
+#define pii pair<ll,ll>
 #define F first
 #define S second
+#define all(x) x.begin(),x.end()
 #define Yes cout<<"Yes\n"
 #define No cout<<"No\n"
-#define pii pair<ll,ll>
 #define hoshino ios::sync_with_stdio(0)
 #define orz cin.tie(0)
 #define osz cout.tie(0)
+#define INF 1e9
+#define MOD 1e9+7
 
 inline void solve(){
     
@@ -23,9 +26,8 @@ signed main(){
     int T = 1;
     //cin>>T;
     for(int i = 0; i<T; i++){
-        //cout<<"Case #"<<i+1<<'\n';
+        // cout<<"Case #"<<i+1<<'\n';
         solve();
     }
     return 0;
 }
-
